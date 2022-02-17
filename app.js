c31bc0af883974c5a7cfb051af7f9e991f53e59d
Daniel Ramirez <zoerunner@gmail.com>
2022-02-17 10:54:41 -0800

    Changes from encryption to md5 hashing

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ require("dotenv").config();
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+// const encrypt = require("mongoose-encryption");
+const md5 = require("md5")
 
 const app = express();
 
@@ -19,7 +20,7 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String,
 });
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
+// userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
 
 const User = mongoose.model("User", userSchema);
 
@@ -34,7 +35,7 @@ app
   })
   .post(function (req, res) {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({ email: username }, function (err, foundUser) {
       if (!err && foundUser) {
@@ -53,7 +54,7 @@ app
   .post(function (req, res) {
     const newUser = new User({
       email: req.body.username,
-      password: req.body.password,
+      password: md5(req.body.password),
     });
     newUser.save(function (err) {
       if (err) {
